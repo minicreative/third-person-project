69a6ed498ed8946f3e08a5657f46d95b7c32c83a
Tomas Roy <hello@minicreative.net>
2022-08-13 19:35:28 -0500

    main styles configured, homepage content & sample page content added

--- index.md
+++ index.md
@@ -8,7 +8,16 @@ header_images:
 - "/uploads/Travel%20by%20land,%20Wilmington%20scene,%20Leslie_s%20Popular%20Monthly%20of%20April%201877.png"
 - "/uploads/Travel%20by%20water,%20Leslie_s%20Popular%20Monthly%20of%20April%201877.png"
 - "/uploads/TPP_B1_F1_Third%20Street,%20Wilmington,%20NC%20-%20postcard%20-%20nothing%20on%20back,%20undated.jpg"
+header_text: Third Person Project, a 501(c)(3) non-profit initiative, was founded in 2015 by&#8212;as The New Yorker described us&#8212;"a loose-knit band of writers, teachers, scholars, and local history buffs." From those vague beginnings, TPP has evolved into one of the most innovative, community-driven research initiatives in the South.
+home_features_header: Explore our work
+home_features:
+- title: Projects
+  description: Here's a description of the projects portion of the website.
+  image: "/uploads/Travel%20by%20water,%20Leslie_s%20Popular%20Monthly%20of%20April%201877.png"
+  link: "/projects"
+- title: Archives
+  description: Here's a description of the archives portion of the website.
+  image: "/uploads/Wilmington%20Cotton%20Compress%20-%20postcard%20front.png"
+  link: "/archives"
 layout: home
 ---
-
-Welcome to the Third Person Project
---
+++ js/main.js
@@ -0,0 +1,169 @@
+
+// On Page Load
+$(document).ready(function() {
+    setupSlider()
+    setColumnWidth()
+})
+
+// On Stricter Page Load
+$(window).on('load', function () {
+	setupBricks()
+})
+
+// On Page Resize
+$(window).resize(function () {
+	setColumnWidth()
+})
+
+function setupSlider() {
+    $("#slider").royalSlider({
+        imageScaleMode: 'fill',
+        transitionType: 'fade',
+        transitionSpeed: 1200,
+        sliderDrag: false,
+        navigateByClick: false,
+        autoplay: {
+            enabled: true,
+            pauseOnHover: false,
+            delay: 4000
+        }
+    })
+}
+
+function setupMobileNav() {
+	let navigation = $(".navigation");
+	let navUnderlay = $("#nav-underlay");
+	$("#nav-toggle").click(function () {
+		if (navigation) navigation.toggleClass("show");
+		if (navUnderlay) navUnderlay.toggleClass("show");
+	});
+	navUnderlay.click(function () {
+		navigation.toggleClass("show");
+		navUnderlay.toggleClass("show");
+	});
+}
+
+function setColumnWidth() {
+	$(".columns").each(function () {
+
+		// Get container
+		let columns = $(this);
+
+		// Get window width
+		let windowWidth = $(window).width();
+
+		// Make mobile & table flags
+		let isMobile = (windowWidth < 760);
+		let isTablet = (windowWidth < 1020);
+
+		// Handle each column
+		columns.find(".column").each(function () {
+
+			// Get column
+			let column = $(this);
+
+			// Reset existing inline margin
+			column.css("marginRight", "");
+
+			// Override on mobile
+			if (isMobile) {
+				column.css("width", "100%");
+				column.css("marginRight", 0);
+				return;
+			}
+	
+			// Get and format margin for math
+			let margin = parseInt(column.css("marginRight").replace('px',''));
+
+			// Setup main/side cols
+			if (column.hasClass("main")) {
+				column.css("width", ((columns.width()-(margin))*0.69));
+				if ((((column.index()+1) % 2) == 0)) {
+					column.css("marginRight", 0);
+				}
+			}
+			else if (column.hasClass("side")) {
+				column.css("width", ((columns.width()-(margin))*0.3));
+				if ((((column.index()+1) % 2) == 0)) {
+					column.css("marginRight", 0);
+				}
+			}
+	
+			// Set columns to half width
+			else if (column.hasClass("half") || (column.hasClass("third") && isTablet)) {
+				column.css("width", ((columns.width()-margin)/2)-3);
+				if ((((column.index()+1) % 2) == 0)) {
+					column.css("marginRight", 0);
+				}
+			}
+
+			// Set columns to third width
+			else if (column.hasClass("third") || column.hasClass("tabletThird")) {
+				column.css("width", ((columns.width()-(margin*2))/3));
+				if ((((column.index()+1) % 3) == 0)) {
+					column.css("marginRight", 0);
+				}
+			}
+
+			// Set columns to fourth width
+			else if (column.hasClass("fourth")) {
+				column.css("width", ((columns.width()-(margin*3))/4));
+				if ((((column.index()+1) % 4) == 0)) {
+					column.css("marginRight", 0);
+				}
+			}
+				
+		})
+	})
+}
+
+let bricks;
+function setupBricks() {
+
+	// Only setup if Bricks included
+	if (!window.Bricks) return;
+
+	// Only setup if bricks area found
+	let hasBricks = false;
+	let firstColumn;
+	$(".columns").each(function () {
+		if ($(this).hasClass("bricks")) {
+			hasBricks = true;
+			firstColumn = $(this).find(".column").first();
+		}
+	})
+	if (!hasBricks) return;
+
+	// Detect column width
+	let numColumns = 1;
+	if (firstColumn.hasClass('fourth'))
+		numColumns = 4;
+	else if (firstColumn.hasClass('third') || firstColumn.hasClass('tabletThird'))
+		numColumns = 3;
+	else if (firstColumn.hasClass('half'))
+		numColumns = 2;
+
+	// Setup sizing
+	let size = {
+		columns: numColumns,
+		gutter: parseInt(firstColumn.css("marginRight").replace('px', ''))
+	};
+	let windowWidth = $(window).width();
+	if (windowWidth < 760) {
+		size.columns = 1;
+		size.gutter = 20;
+	} else if (windowWidth < 1020) {
+		size.columns = 2;
+	}
+
+	// Setup bricks
+	bricks = Bricks({
+		container: '.columns',
+		packed: 'data-packed',
+		sizes: [
+			size
+		],
+		position:true,
+	});
+	bricks.pack();
+}
